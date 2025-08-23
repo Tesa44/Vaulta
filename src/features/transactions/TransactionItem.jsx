d7f3d97d2f7c516403d7f7b107642542157b6962
Tesa44 <mateusz.tesarewicz123@gmail.com>
2025-08-23 17:05:21 +0200

    style transaction item with new arrow icons

--- src/features/transactions/TransactionItem.jsx
+++ src/features/transactions/TransactionItem.jsx
@@ -1,29 +1,35 @@
 import styles from "./TransactionItem.module.css";
+import {
+  ArrowBendDownRightIcon,
+  ArrowBendDownLeftIcon,
+} from "@phosphor-icons/react";
 import { formatCurrency } from "../../utils/formatCurrency";
 import { formatDate } from "../../utils/formatDate";
 import { useUserAccounts } from "../../contexts/UserAccountsContext";
 
 function TransactionItem({ transaction }) {
   const { currentAccount } = useUserAccounts();
+  const isPositive = transaction.amount >= 0;
 
   return (
-    <li
-      className={`${styles.transactionItem} ${
-        transaction.amount >= 0 ? styles["green"] : styles["red"]
-      }`}
-    >
+    <li className={styles.transactionItem}>
       <div className={styles.transactionInfo}>
-        <h4 className={styles.transactionTitle}>{transaction.title}</h4>
-        <p className={styles.transactionTitle}>{transaction.name}</p>
+        <h4>{transaction.title}</h4>
+        <p>{transaction.name}</p>
         <span className={styles.transactionDate}>
           {formatDate(transaction.date)}
         </span>
       </div>
       <div className={styles.transactionAmount}>
-        {formatCurrency(transaction.amount, currentAccount.currency)}
+        <p>{formatCurrency(transaction.amount, currentAccount.currency)}</p>
+        {isPositive ? (
+          <ArrowBendDownLeftIcon size={32} color="#40c057" />
+        ) : (
+          <ArrowBendDownRightIcon size={32} color="#f03e3e" />
+        )}
       </div>
       <div className={styles.balanceAfter}>
-        <p>Balance after transaction</p>{" "}
+        <p>Balance after transaction</p>
         <span>
           {formatCurrency(transaction.balanceAfter, currentAccount.currency)}
         </span>
