6695d1a337abc09141faf0bc26067791332445ab
Tesa44 <mateusz.tesarewicz123@gmail.com>
2025-08-24 18:07:25 +0200

    style new transfer layout, add Loader, ErrorMessage, fix list overflow

--- src/features/transfer/NewTransfer.jsx
+++ src/features/transfer/NewTransfer.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./NewTransfer.module.css";
-import UserInfo from "./ReceiverInfo";
+import ReceiverInfo from "./ReceiverInfo";
 import NewTransferForm from "./NewTransferForm";
 import BackButton from "../../ui/BackButton";
+import Loader from "../../ui/Loader";
+import ErrorMessage from "../../ui/ErrorMessage";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -12,6 +14,7 @@ function NewTransfer() {
   const [userAccounts, setUserAccounts] = useState([]);
   const [toAccount, setToAccount] = useState({});
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const { id } = useParams();
 
@@ -19,13 +22,17 @@ function NewTransfer() {
     function () {
       async function fetchUser() {
         setLoading(true);
+        setError("");
         try {
           const res = await fetch(`${BASE_URL}/users?id=${id}`);
 
+          if (!res.ok) {
+            throw new Error("Something went wrong with fetching user data");
+          }
           const [data] = await res.json();
           setUser(data);
         } catch (err) {
-          console.error(err.message);
+          setError(err.message);
         } finally {
           setLoading(false);
         }
@@ -39,13 +46,18 @@ function NewTransfer() {
     function () {
       async function fetchAccounts() {
         setLoading(true);
+        setError("");
         try {
           const res = await fetch(`${BASE_URL}/accounts?userId=${id}`);
+
+          if (!res.ok) {
+            throw new Error("Something went wrong with fetching accounts data");
+          }
 
           const data = await res.json();
           setUserAccounts(data);
         } catch (err) {
-          console.error(err.message);
+          setError(err.message);
         } finally {
           setLoading(false);
         }
@@ -56,18 +68,22 @@ function NewTransfer() {
   );
 
   return (
-    <div className={styles.userDetails}>
+    <div className={styles.container}>
       <div className={styles.header}>
         <h3>Fast transfer</h3>
         <BackButton></BackButton>
       </div>
-      <div className={styles.container}>
-        <UserInfo
-          user={loading ? "loading..." : user}
-          accounts={userAccounts}
-          onSelect={setToAccount}
-          selected={toAccount}
-        ></UserInfo>
+      <div className={styles.main}>
+        {loading && <Loader></Loader>}
+        {error && <ErrorMessage></ErrorMessage>}
+        {!loading && !error && (
+          <ReceiverInfo
+            user={user}
+            accounts={userAccounts}
+            onSelect={setToAccount}
+            selected={toAccount}
+          ></ReceiverInfo>
+        )}
         <NewTransferForm
           toAccount={toAccount}
           receiverFullname={`${user?.name} ${user?.surname}`}
--- src/features/transfer/ReceiverInfo.jsx
+++ src/features/transfer/ReceiverInfo.jsx
@@ -2,7 +2,7 @@ import styles from "./ReceiverInfo.module.css";
 
 function ReceiverInfo({ user, accounts, onSelect, selected }) {
   return (
-    <div className={styles.userInfo}>
+    <div className={styles.receiverInfo}>
       <div className={styles.box}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -13,27 +13,20 @@ function ReceiverInfo({ user, accounts, onSelect, selected }) {
         >
           <path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path>
         </svg>
-        <p>{user ? user.name : "..."}</p>
-        <p>{user ? user.surname : "..."}</p>
+        <p>{user.name}</p>
+        <p>{user.surname}</p>
       </div>
-      <div className={styles.listBox}>
-        {!user.name && <h4>Loading...</h4>}
-        {user.name && accounts.length > 0 && (
-          <h4>{`${user.name}'s accounts`}</h4>
-        )}
-        {user.name && accounts.length === 0 && (
-          <h4>{`${user.name} has no accounts`}</h4>
-        )}
-        <ul className={styles.list}>
-          {accounts.map((acc) => (
-            <AccountItem
-              account={acc}
-              onSelect={onSelect}
-              isActive={acc.id === selected?.id}
-            ></AccountItem>
-          ))}
-        </ul>
-      </div>
+      {accounts.length > 0 && <h4>{`${user.name}'s accounts`}</h4>}
+      {accounts.length === 0 && <h4>{`${user.name} has no accounts`}</h4>}
+      <ul className={styles.list}>
+        {accounts.map((acc) => (
+          <AccountItem
+            account={acc}
+            onSelect={onSelect}
+            isActive={acc.id === selected?.id}
+          ></AccountItem>
+        ))}
+      </ul>
     </div>
   );
 }
